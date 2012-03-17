cc7edcd36455baa80d6cb629c25abfb142ea4421
nqounet <nqou.net@gmail.com>
2012-03-17 14:33:57 +0900

    X-PJAX: OFF で通らないようにした

--- app.pl
+++ app.pl
@@ -5,7 +5,7 @@ use Mojolicious::Lite;
 
 under sub {
   my $self = shift;
-  $self->layout( $self->req->headers->header('X-PJAX') ? 'pjax' : 'default' );
+  $self->layout( $self->req->headers->header('X-PJAX') eq 'true' ? 'pjax' : 'default' );
   1;
 };
 
